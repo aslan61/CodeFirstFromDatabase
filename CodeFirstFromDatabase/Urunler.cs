c2242448701a66215dd15cbf76b211e67e0dc39f
Ahmet Aslan <simsar6155@gmail.com>
2025-06-04 22:31:28 +0300

    Proje dosyası ekle.

---
+++ CodeFirstFromDatabase/Urunler.cs
@@ -0,0 +1,26 @@
+namespace CodeFirstFromDatabase
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Urunler
+    {
+        [Key]
+        public int UrunID { get; set; }
+
+        public string UrunAd { get; set; }
+
+        public string UrunMarka { get; set; }
+
+        public string UrunKategori { get; set; }
+
+        public string UrunStok { get; set; }
+
+        public int? Kategori_KategoriID { get; set; }
+
+        public virtual Kategori Kategori { get; set; }
+    }
+}
